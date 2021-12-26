81dcecd800e8f8cea2bac1719ce85cdda2aaa363
Helge Ahrens <helgeahrens@MacBook-Pro.local>
2021-12-26 15:47:44 +0100

    added tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -4,12 +4,18 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+  ScrollRestoration,
+} from 'remix';
+import type { MetaFunction } from 'remix';
+
+import styles from './tailwind.css';
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: 'New Remix App' };
 };
 
 export default function App() {
@@ -25,7 +31,7 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
     </html>
   );
